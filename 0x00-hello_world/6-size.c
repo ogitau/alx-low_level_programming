e3f7dd7277c84fbaad686caed659c508b121b9d4
Owen Gitau <owengitau02@gmail.com>
2023-06-15 07:28:45 -0700

    task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -4,16 +4,16 @@
  * main -starting point of the program
  * Description -'the program will print the bit values of the variables'
  *
- * return -always zero 0
+ * Return: always zero 0
  */
 
 int main(void)
 {
-	printf("Size of char: %zu bytes\n", sizeof(char));
-	printf("Size of int: %zu bytes\n", sizeof(int));
-	printf("Size of float: %zu bytes\n", sizeof(float));
-	printf("Size of double: %zu bytes\n", sizeof(double));
-	printf("Size of long: %zu bytes\n", sizeof(long));
-	printf("Size of long long: %zu bytes\n", sizeof(long long));
+	printf("Size of char: %zu byte(s)\n", sizeof(char));
+	printf("Size of int: %zu byte(s)\n", sizeof(int));
+	printf("Size of float: %zu byte(s)\n", sizeof(float));
+	printf("Size of double: %zu byte(s)\n", sizeof(double));
+	printf("Size of long: %zu byte(s)\n", sizeof(long));
+	printf("Size of long long: %zu byte(S)\n", sizeof(long long));
 		return (0);
 }
