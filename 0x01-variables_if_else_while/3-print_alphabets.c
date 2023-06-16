f2a0bc3cdd1a58232fab0af4a1724bf3171bb72d
Owen Gitau <owengitau02@gmail.com>
2023-06-16 01:56:59 -0700

    alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - starting point
+ *
+ * Return: always 0 on success
+ */
+int main(void)
+{
+	char a;
+
+	for (a = 'a'; a <= 'z' ; a++)
+		putchar(a);
+	for (a = 'A'; a <= 'Z' ; a++)
+		putchar(a);
+	putchar('\n');
+	return (0);
+}
