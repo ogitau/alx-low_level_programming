1dcd8a1c9ee884db3dab776bd0ce2d871f4b9b8e
Owen Gitau <owengitau02@gmail.com>
2023-06-16 02:27:09 -0700

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - starting point
+ *
+ * Return: always 0 on success
+ */
+int main(void)
+{
+	int a = 0;
+
+	for (; a < 10 ; a++)
+		printf("%d", a);
+	putchar('\n');
+	return (0);
+}
