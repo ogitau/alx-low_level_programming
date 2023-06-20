90489c09c660f4e9cebaf6296150cf27fcf5b66f
Owen Gitau <owengitau02@gmail.com>
2023-06-20 12:03:01 -0700

    a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - the main function
+ *
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: sum of both integers
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
