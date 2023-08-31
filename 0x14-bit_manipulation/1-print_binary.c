1f41b52fcc2e807040fcb4f314990ebee3e2e6ca
Owen Gitau <owengitau02@gmail.com>
2023-08-31 04:44:56 -0700

    Print Binary

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -2,35 +2,12 @@
 #include <stdio.h>
 
 /**
- * print_binary - function to print binary
- * @n: long int of type unsigned
- * Return: 0 on success
+ * print_binary - function to print binary numbers
+ * @n: the number printed in binary
  */
-
 void print_binary(unsigned long int n)
 {
-	unsigned long int check;
-	unsigned int size = sizeof(n) * 8;
-	unsigned int count;
-	unsigned int flag = 1;
-
-	count = 0;
-	while (count < size)
-	{
-		check = (n << 1);
-		check >>= 1;
-		if (n != check)
-		{
-			flag = 0;
-			write(1, "1", 1);
-		}
-		else if (!flag)
-		{
-			write(1, "0", 1);
-		}
-		n <<=  1;
-		count++;
-	}
-	if (flag == 1)
-		write(1, "0", 1);
+	if (n > 1)
+		print_binary(n >> 1);
+	_putchar((n & 1)  + '0');
 }
