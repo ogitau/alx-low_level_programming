ce2642a47eaffda6e1b0690ca5085a2fa7227e7f
Owen Gitau <owengitau02@gmail.com>
2023-09-04 13:05:49 -0700

    Read text file

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,36 +1,39 @@
 #include "main.h"
 /**
- * read_textfile - function to read and write a file
- * @filename: const char type pointer to file to be read
- * @letters: size_t type
- * Return: always successful
+ * read_textfile - reads a text file and prints it to the POSIX standard output
+ * @filename: name of the file to read
+ * @letters: number of letters it should read and print
+ *
+ * Return: actual number of letters it could read and print
+ *         0 if the filename is NULL
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
-	ssize_t fdread, fdwrite, fdclose;
-	char *space;
+	ssize_t len_char_r, len_char_w;
+	char *buffer;
 
 	if (filename == NULL)
 		return (0);
-	space = malloc(sizeof(char) * letters);
-	if (space == NULL)
-	{
-		return (-1);
-	}
-
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
-	fdread = read(fd, space, letters);
-	if (fdread == -1)
-		return (-1);
-	fdwrite = write(STDOUT_FILENO, space, fdread);
-
-	if (fdwrite == -1)
-		return (-1);
-	fdclose = close(fd);
-	if (fdclose == -1)
-		return (-1);
-	return (fdread);
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+	{
+		close(fd);
+		return (0);
+	}
+	len_char_r = read(fd, buffer, letters);
+	close(fd);
+	if (len_char_r == -1)
+	{
+		free(buffer);
+		return (0);
+	}
+	len_char_w = write(STDOUT_FILENO, buffer, len_char_r);
+	free(buffer);
+	if (len_char_r != len_char_w)
+		return (0);
+	return (len_char_w);
 }
