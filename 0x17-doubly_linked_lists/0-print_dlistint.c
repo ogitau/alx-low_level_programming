588e8f1f5bd43798ae77fda7d9f52fead05c968d
Owen Gitau <owengitau02@gmail.com>
2023-11-09 00:56:40 -0800

    print list elements

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+/**
+ * print_dlistint - function to print elements of the doubly linked list
+ * @h: pointer to the head node
+ * Return: the number of nodes
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	int index;
+
+	index = 0;
+
+	if (h == NULL)
+		return (index);
+	while (h->prev != NULL)
+		h = h->prev;
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		index++;
+		h = h->next;
+	}
+	return (index);
+}
